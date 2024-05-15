e30761446b860b22ee0ed6399bf0c4d7101d71dd
cpy0126 <0126morris@gmail.com>
2024-05-16 02:23:15 +0800

    add MCLRec config for each dataset

---
+++ train_mclrec.sh
@@ -0,0 +1,3 @@
+python run.py --dataset amazon_beauty --model MCLRec --config_file config/model/MCLRec_beauty.yaml
+python run.py --dataset amazon_sport --model MCLRec --config_file config/model/MCLRec_sport.yaml
+python run.py --dataset ml-1m --model MCLRec --config_file config/model/MCLRec_ml.yaml
